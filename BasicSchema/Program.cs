3331007fa4f641b5f58db4b9dee61ee0028ce170
lsodis <vova2010201040@gmail.com>
2023-03-09 20:31:03 +0300

    Complete task

--- BasicSchema/Program.cs
+++ BasicSchema/Program.cs
@@ -1,6 +1,12 @@
+using BasicSchema.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+
+builder.Services.AddScoped<CartService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
